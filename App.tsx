e4db0aad7fb8509cc563dd8405567cbe3ab00553
tamarkohan <tamarkohan@gmail.com>
2026-06-09 15:32:37 +0300

    Add SETUP.md and working home dashboard

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import HomeDashboard from "./src/screens/HOMEDASHBOARDEMERGENCYALERTS";
+import HomeDashboard from "./src/screens/HomeDashboard";
 
 const Stack = createNativeStackNavigator();
 
