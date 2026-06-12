d48553d5286fdc6d0408e224deb7f92977257d45
tamarkohan <tamarkohan@gmail.com>
2026-06-12 16:11:02 +0300

    Login Screen

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoginScreen from "./src/screens/LoginScreen";
 import HomeDashboard from "./src/screens/HomeDashboard";
 
 const Stack = createNativeStackNavigator();
@@ -11,6 +12,7 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Home" component={HomeDashboard} />
         </Stack.Navigator>
       </NavigationContainer>
